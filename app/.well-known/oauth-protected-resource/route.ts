ed376caa7329eb01ad887ea1478dbe1a049fac15
Michael Hadley <m@mthadley.com>
2025-05-08 15:15:19 -0700

    Implement basic `withAuthkit` and `.well-known` metadata

---
+++ app/.well-known/oauth-protected-resource/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest } from "next/server";
+
+export function GET(_request: NextRequest): Response {
+  const mcpServerDomain =
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000";
+  const protocol = mcpServerDomain.startsWith("localhost") ? "http" : "https";
+
+  return new Response(
+    JSON.stringify({
+      resource: `${protocol}://${mcpServerDomain}`,
+      authorization_servers: [`https://${process.env.AUTHKIT_DOMAIN}`],
+      bearer_methods_supported: ["header"],
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+}
---
+++ lib/with-authkit.ts
@@ -0,0 +1,84 @@
+import { NextRequest } from "next/server";
+import * as jose from "jose";
+
+export interface Authorization {
+  accessToken: string;
+  claims: {
+    iss: string;
+    aud: string;
+    sub: string;
+    sid: string;
+    jti: string;
+  };
+}
+
+export function withAuthkit(
+  next: (request: NextRequest) => Promise<Response>,
+): (request: NextRequest) => Promise<Response> {
+  const authkitDomain = process.env.AUTHKIT_DOMAIN;
+
+  const jwks = jose.createRemoteJWKSet(
+    new URL(`https://${authkitDomain}/oauth2/jwks`),
+  );
+
+  const mcpServerDomain =
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000";
+  const protocol = mcpServerDomain.startsWith("localhost") ? "http" : "https";
+
+  const wwwAuthenticateHeader = [
+    'Bearer error="unauthorized"',
+    'error_description="Authorization needed"',
+    `resource_metadata="${protocol}://${mcpServerDomain}/.well-known/oauth-protected-resource"`,
+  ].join(", ");
+
+  const unauthorized = (error: string) =>
+    new Response(JSON.stringify({ error }), {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": wwwAuthenticateHeader,
+        "Content-Type": "application/json",
+      },
+    });
+
+  return async (request: NextRequest) => {
+    const authorizationHeader = request.headers.get("Authorization");
+    if (!authorizationHeader) {
+      return unauthorized("Missing Authorization Header");
+    }
+
+    const [scheme = "", token] = authorizationHeader.split(" ");
+    if (!/^Bearer$/i.test(scheme) || !token) {
+      return unauthorized("Invalid Authorization Header");
+    }
+
+    let payload: Authorization["claims"];
+    try {
+      ({ payload } = await jose.jwtVerify(token, jwks, {
+        issuer: `https://${authkitDomain}`,
+      }));
+    } catch (error) {
+      if (
+        error instanceof jose.errors.JWTExpired ||
+        error instanceof jose.errors.JWKSInvalid
+      ) {
+        return unauthorized("Invalid or expired access token");
+      }
+
+      if (error instanceof jose.errors.JOSEError) {
+        console.error("Error initializing JWKS", { error });
+
+        return new Response("Internal server error", { status: 500 });
+      }
+
+      throw error;
+    }
+
+    // TODO
+    //(req as unknown as { auth: Authorization }).auth = {
+    //  accessToken: token,
+    //  claims: payload,
+    //};
+
+    return next(request);
+  };
+}
