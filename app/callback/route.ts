3fee8d07dd3f7246d7fe90a9ce6d39295a37ad01
Dan Dorman <dan@workos.com>
2025-05-09 13:47:11 -0600

    Redirect to sign-in page on authentication error

--- app/callback/route.ts
+++ app/callback/route.ts
@@ -1,3 +1,12 @@
-import { handleAuth } from "@workos-inc/authkit-nextjs";
+import { handleAuth, getSignInUrl } from "@workos-inc/authkit-nextjs";
+import { redirect } from "next/navigation";
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  onError: async (error) => {
+    console.error("Error authenticating", error);
+
+    // TODO: Should probably do something more than just redirect back to the
+    // sign-in page (without even an error message!)
+    redirect(await getSignInUrl());
+  },
+});
