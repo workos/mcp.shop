c0170f2871f84f4ef7955325192ebb18df0d5ac8
Cameron Matheson <cameron@workos.com>
2025-05-13 15:15:21 -0600

    Remove dice-roller

--- app/[transport]/route.ts
+++ app/[transport]/route.ts
@@ -1,38 +1,11 @@
+import { placeOrder } from "@/lib/orders";
 import { withAuthkit } from "@/lib/with-authkit";
 import createMcpHandler from "@vercel/mcp-adapter/next";
 import { z } from "zod";
-import redis from "@/lib/redis";
 
 const handler = withAuthkit((request, auth) =>
   createMcpHandler(
     (server) => {
-      server.tool(
-        "roll",
-        "Roll a given number of 6-sided dice. Returns the individual rolls and the list of rolls. If the user does not specify a number of dice, use 1. If the user does not specify a number of sides, use 6.",
-        { dice: z.number(), sides: z.number() },
-        async ({ dice, sides }) => {
-          const rolls = [];
-          let sum = 0;
-          for (let i = 0; i < dice; i++) {
-            const roll = Math.floor(Math.random() * sides) + 1;
-            rolls.push(roll);
-            sum += roll;
-          }
-          return {
-            content: [
-              {
-                type: "text",
-                text: JSON.stringify({ rolls, sum }),
-              },
-              {
-                type: "text",
-                text: `These rolls were generated for user with ID ${auth.claims.sub}`,
-              },
-            ],
-          };
-        },
-      );
-
       server.tool(
         "listMcpShopInventory",
         "Returns a list of the items for sale at mcp.shop. " +
