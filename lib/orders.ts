89713896095e7691906aa2c6817b89f15f02ea56
Cameron Matheson <cameron@workos.com>
2025-05-13 16:54:15 -0600

    Add listOrders function

--- lib/orders.ts
+++ lib/orders.ts
@@ -1,6 +1,18 @@
 import redis from "@/lib/redis";
 import { User } from "./with-authkit";
 
+export interface Order extends HSETObject {
+  id: number;
+  userId: User["id"];
+  orderDate: string;
+  sku: "MCP-NTSHRT-25-GW01";
+  firstName: string;
+  lastName: string;
+  email: string;
+  company: string;
+  tshirtSize: string;
+}
+
 export const placeOrder = async (
   args: {
     company: string;
@@ -11,9 +23,10 @@ export const placeOrder = async (
 ) => {
   const orderId = await redis.incr("order:id:counter");
 
-  const order = {
+  const order: Order = {
     id: orderId,
     userId: user.id,
+    orderDate: new Date().toUTCString(),
     sku: "MCP-NTSHRT-25-GW01",
     firstName: user.firstName ?? "UNKNOWN",
     lastName: user.lastName ?? "UNKNOWN",
@@ -22,7 +35,23 @@ export const placeOrder = async (
     tshirtSize: args.tshirtSize,
   };
 
-  await redis.hSet(`orders:${orderId}`, order);
+  await redis.hSet(`orders:${user.id}:${orderId}`, { ...order });
 
   return order;
+};
+
+export const getOrders = async (user: User): Promise<Order[]> => {
+  const orders: Order[] = [];
+  let cursor = 0;
+  do {
+    const orderKeys = await redis.scan(cursor, {
+      MATCH: `orders:${user.id}:*`,
+      COUNT: 100,
+    });
+    for (const key of orderKeys.keys) {
+      orders.push((await redis.hGetAll(key)) as unknown as Order);
+    }
+    cursor = orderKeys.cursor;
+  } while (cursor !== 0);
+  return orders;
 };
