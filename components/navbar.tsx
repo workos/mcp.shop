d5906275c354f3c37400efac1999162e106cca48
Dan Dorman <dan@workos.com>
2025-05-14 16:21:53 -0700

    Hide 'welcome back' on mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,14 +10,18 @@ export async function Navbar() {
     <nav className="flex items-center justify-between p-4 text-neutral-400">
       <Link className="flex" href="/">
         <Image alt="MCP Shop logo" src="/logo.png" height={40} width={40} />
-        <div className="flex w-full items-center font-bold text-lg pl-1">mcp.shop</div>
+        <div className="flex w-full items-center font-bold text-lg pl-1">
+          mcp.shop
+        </div>
       </Link>
       <div>
         {user ? (
           <div className="flex gap-2 items-center">
             <Link className="flex gap-2 items-center" href="/orders">
               <ShoppingCartIcon className="h-5 border rounded m-1 text-foreground" />
-              <div>Welcome back, {user.firstName ?? user.email}.</div>
+              <div className="hidden lg:block">
+                Welcome back, {user.firstName ?? user.email}.
+              </div>
             </Link>
             <form
               className="inline"
