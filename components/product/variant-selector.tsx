10cc7426c57f26eb571a5fd36595972de2ad7de4
Tobin South <tobin.south@gmail.com>
2025-05-14 13:48:44 -0700

    Garrett design inspriation

--- components/product/variant-selector.tsx
+++ components/product/variant-selector.tsx
@@ -43,7 +43,7 @@ export function VariantSelector({
     <form key={option.id}>
       <dl className="mb-8">
         <dt className="mb-4 text-sm uppercase tracking-wide">{option.name}</dt>
-        <dd className="flex flex-wrap gap-3">
+        <dd className="grid grid-cols-4 sm:grid-cols-4 gap-3">
           {option.values.map((value) => {
             const optionNameLowerCase = option.name.toLowerCase();
 
