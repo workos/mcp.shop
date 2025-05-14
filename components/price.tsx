c02df2fe15bc7e073b3a16a9434d8988e63a7e59
Tobin South <tobin.south@gmail.com>
2025-05-14 11:09:33 -0700

    Adding non-money prices

--- components/price.tsx
+++ components/price.tsx
@@ -12,14 +12,7 @@ const Price = ({
   currencyCodeClassName?: string;
 } & React.ComponentProps<"p">) => (
   <p suppressHydrationWarning={true} className={className}>
-    {`${new Intl.NumberFormat(undefined, {
-      style: "currency",
-      currency: currencyCode,
-      currencyDisplay: "narrowSymbol",
-    }).format(parseFloat(amount))}`}
-    <span
-      className={clsx("ml-1 inline", currencyCodeClassName)}
-    >{`${currencyCode}`}</span>
+    {amount}
   </p>
 );
 
