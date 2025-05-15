dfec91fd8a07671e999911ab1768fdb1ff9d2983
Cameron Matheson <cameron@workos.com>
2025-05-15 12:18:55 -0600

    delete comments

--- app/[transport]/route.ts
+++ app/[transport]/route.ts
@@ -28,20 +28,6 @@ const handler = withAuthkit((request, auth) =>
                       image_url:
                         "https://mcp.shop/shirt.jpg",
                     },
-                    // {
-                    //   label: "Mug",
-                    //   description: products.mug.descriptionHtml,
-                    //   image_url:
-                    //     "http://localhost:3000/_next/image?url=%2Fmug.webp&w=1200&q=75",
-                    //   status: "Out of stock",
-                    // },
-                    // {
-                    //   label: "Beanie",
-                    //   description: products.beanie.descriptionHtml,
-                    //   image_url:
-                    //     "http://localhost:3000/_next/image?url=%2Fbeanie.webp&w=1200&q=75",
-                    //   status: "Out of stock",
-                    // },
                   ],
                 }),
               },
