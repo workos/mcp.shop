0b2457a32eb615d8d45d1d3b1d43d89279ca8d64
Julie Goodman <152425034+Joyadelunita@users.noreply.github.com>
2025-05-20 15:54:14 -0600

    Updating product page layout

--- components/product/product-page.tsx
+++ components/product/product-page.tsx
@@ -7,27 +7,25 @@ import { Suspense } from "react";
 import { ProductProvider } from "@/components/product/product-context";
 
 export default async function ProductPage() {
-  const product = await getProduct('shirt');
+  const product = await getProduct("shirt");
 
   if (!product) return notFound();
 
   return (
     <ProductProvider>
-      <div className="mx-auto max-w-[--breakpoint-2xl] px-4">
-        <div className="flex flex-col rounded-lg border p-8 md:p-12 lg:flex-row lg:gap-0 border-neutral-800 bg-black justify-center lg:rounded-none lg:border-none">          
-        <div className="h-full w-full basis-full lg:basis-1/2 lg:mr-8 lg:max-h-[600px] overflow-y-auto scrollbar-hide mb-10">
-          <Suspense
+      <div className="mx-auto max-w-[1440] px-4">
+        <div className="flex flex-col rounded-lg border p-8 md:p-12 lg:flex-row lg:gap-0 border-neutral-800 justify-center lg:rounded-none lg:border-none">
+          <div className="h-full w-full basis-full lg:basis-1/2 lg:mr-8 lg:max-h-[600px] overflow-y-auto scrollbar-hide mb-10">
+            <Suspense
               fallback={
-                <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden" />
+                <div className="relative aspect-square h-full w-full overflow-hidden" />
               }
             >
               <Gallery
-                images={product.images
-                  .slice(0, 5)
-                  .map((image: ProductImage) => ({
-                    src: image.url,
-                    altText: image.altText,
-                  }))}
+                images={product.images.map((image: ProductImage) => ({
+                  src: image.url,
+                  altText: image.altText,
+                }))}
               />
             </Suspense>
           </div>
