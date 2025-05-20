6fc8277352603b4434aa8ea7906ad350909c67f4
Julie Goodman <152425034+Joyadelunita@users.noreply.github.com>
2025-05-20 16:37:52 -0600

    Added gradient background

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -9,7 +9,13 @@ export function ProductDescription({ product }: { product: Product }) {
     <>
       <div className="mb-6 flex flex-col border-b pb-6 border-neutral-700">
         <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
-        <div className="inline-block self-start bg-neutral-800 text-gray-200 text-lg font-normal px-4 py-1 rounded-full shadow">
+        <div
+          className="inline-block self-start text-gray-200 text-lg font-normal px-4 py-1 rounded-full shadow"
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(255,126,95,0.15) 0%, rgba(254,180,123,0.15) 100%)",
+          }}
+        >
           <Price
             amount={product.priceRange.maxVariantPrice.amount}
             currencyCode={product.priceRange.maxVariantPrice.currencyCode}
