90c1001072ca5c76de183ba10b2918fb50efbbc4
Julie Goodman <152425034+Joyadelunita@users.noreply.github.com>
2025-05-20 17:13:27 -0600

    Radix themes dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,12 +29,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-4`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthKitProvider>
-          <Navbar />
-          {children}
-        </AuthKitProvider>
+        <Theme appearance="dark">
+          <AuthKitProvider>
+            <Navbar />
+            {children}
+          </AuthKitProvider>
+        </Theme>
       </body>
     </html>
   );
