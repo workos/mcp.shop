3f57438ef78b403003ec7d649487d51bf7d5926d
zmzlois <lois.z@comcord.vision>
2025-08-01 01:36:02 -0700

    feat: add github and adjust layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaXTwitter, FaLinkedin, FaYoutube } from "react-icons/fa6";
+import { FaXTwitter, FaLinkedin, FaYoutube, FaGithub } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <footer className="w-full border-t border-neutral-800 py-4 px-6 flex items-center justify-between bg-black">
       {/* Logo on the left */}
       <div className="flex items-center">
+        <Link
+          href="https://github.com/workos/mcp.shop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="footer-social-icon w-5 h-5 text-neutral-400 transition" />
+        </Link>
         <Link
           href="https://workos.com/?utm_source=mcp_shop&utm_medium=referral&utm_campaign=workos_mcp"
           target="_blank"
